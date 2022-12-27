fab406c0d12ef3aa4e6a8b688a9ca548ac07615a
He.Camille <camille.he@outlook.com>
2022-12-27 21:55:52 +0800

    feat: render expenses component

--- react-complete-guide/src/components/expenses/Expenses.js
+++ react-complete-guide/src/components/expenses/Expenses.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../Card";
+import ExpenseFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      {props.items.map((expense) => (
-        <ExpenseItem {...expense} />
-      ))}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          filteredYear={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        ></ExpenseFilter>
+        {props.items
+          .filter((item) => item.date.getFullYear().toString() === filteredYear)
+          .map((expense, index) => (
+            <ExpenseItem key={index} {...expense} />
+          ))}
+      </Card>
+    </div>
   );
 };
 
