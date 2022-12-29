36c1bd0825dd62e8a665ae066ad1fedc4bdad2ca
He.Camille <camille.he@outlook.com>
2022-12-29 17:33:10 +0800

    feat: use context api

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,16 +1,25 @@
+import React, { useContext } from "react";
 import "./App.css";
-import UserHome from "./components/User/UserHome";
-import CourseGoal from "./components/CourseGoals/CourseGoal";
-import ExpensesHome from "./components/Expenses/ExpensesHome";
+// import UserHome from "./components/User/UserHome";
+// import CourseGoal from "./components/CourseGoals/CourseGoal";
+// import ExpensesHome from "./components/Expenses/ExpensesHome";
 
-const App = () => {
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
+
+function App() {
+  const ctx = useContext(AuthContext);
   return (
     <>
-      <UserHome />
-      <CourseGoal />
-      <ExpensesHome />
+      <MainHeader />
+      <main>
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
+      </main>
     </>
   );
-};
+}
 
 export default App;
