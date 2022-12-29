6c6a182ad226d4247d26045898035493bdb28cd8
He.Camille <camille.he@outlook.com>
2022-12-29 21:23:17 +0800

    feat: implement main page

---
+++ react-order-app/src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import classes from "./Header.module.css";
+import meals from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={meals} alt="A table of food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ react-order-app/src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummry from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = (props) => {
+  return (
+    <>
+      <MealsSummry />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
