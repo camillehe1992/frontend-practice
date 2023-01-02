ab58f786f0d70d1451ebd17da02d737fd61acebc
He.Camille <camille.he@outlook.com>
2023-01-02 15:20:18 +0800

    add a hook named useHttp

--- react-order-app/src/components/Meals/AvailableMeals.js
+++ react-order-app/src/components/Meals/AvailableMeals.js
@@ -1,17 +1,58 @@
-import Card from "../UI/Card";
+import { useState, useEffect } from "react";
 import classes from "./AvailableMeals.module.css";
-import { DUMMY_MEALS } from "./dummy-meals";
+import Card from "../UI/Card";
 import MealItem from "./MealItem";
+import useHttp from "../../hooks/use-http";
+import { DUMMY_MEALS } from "./dummy-meals";
+
+const API_URL = "https://swapi.dev/api/films";
 
 const AvailableMeals = (props) => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} {...meal} />
-  ));
+  const [meals, setMeals] = useState([]);
+  const { isLoading, error, sendRequest: fetchMeals } = useHttp();
+  useEffect(() => {
+    const transformMeals = (meals) => {
+      const loadedMeals = DUMMY_MEALS.map((item) => {
+        return {
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          price: item.price,
+        };
+      });
+      setMeals(loadedMeals);
+    };
+    fetchMeals(
+      {
+        url: API_URL,
+      },
+      transformMeals
+    );
+  }, [fetchMeals]);
+
+  let content = <p>Found no meals</p>;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (meals.length > 0) {
+    content = (
+      <ul>
+        {meals.map((meal) => (
+          <MealItem key={meal.id} {...meal} />
+        ))}
+      </ul>
+    );
+  }
+
   return (
     <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
+      <Card>{content}</Card>
     </section>
   );
 };
