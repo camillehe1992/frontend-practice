a9646e1b5e428b8a2eb9a54a79454be018e13d90
He.Camille <camille.he@outlook.com>
2023-01-02 21:53:52 +0800

    feat: add tabs to display views

--- react-complete-guide/src/components/Layout/Main/Main.js
+++ react-complete-guide/src/components/Layout/Main/Main.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../../UI/Card";
-// import Navigation from "../Navigation/Navigation";
+import Navigation from "../Navigation/Navigation";
 import classes from "./Main.module.css";
 
-// import Form from "./components/Form/Form";
-// import Counter from "./components/Counter/Counter";
-// import MoviesHome from "./components/Movies/MoviesHome";
-// import UserHome from "./components/User/UserHome";
-// import CourseGoal from "./components/CourseGoals/CourseGoal";
-// import ExpensesHome from "./components/Expenses/ExpensesHome";
+import Counter from "../../../components/Counter/Counter";
+import MoviesHome from "../../../components/Movies/MoviesHome";
+import UserHome from "../../../components/User/UserHome";
+import CourseGoal from "../../../components/CourseGoals/CourseGoal";
+import ExpensesHome from "../../../components/Expenses/ExpensesHome";
 
 const Home = () => {
+  const [activeTab, setActiveTab] = useState("Counter");
+  const views = [
+    "Counter",
+    "MoviesHome",
+    "UserHome",
+    "CourseGoal",
+    "ExpensesHome",
+  ];
+  const changeTab = (event) => {
+    event.preventDefault();
+    setActiveTab(event.target.value);
+  };
+
+  let viewContent = <Counter />;
+  switch (activeTab) {
+    case "MoviesHome":
+      viewContent = <MoviesHome />;
+      break;
+    case "UserHome":
+      viewContent = <UserHome />;
+      break;
+    case "CourseGoal":
+      viewContent = <CourseGoal />;
+      break;
+    case "ExpensesHome":
+      viewContent = <ExpensesHome />;
+      break;
+    default:
+      viewContent = <Counter />;
+      break;
+  }
+
   return (
     <>
+      <Navigation tabs={views} onClick={changeTab} />
       <Card className={classes.content}>
-        <h1>Welcome back!</h1>
+        <h1>Welcome back! View {activeTab}</h1>
+        {viewContent}
       </Card>
     </>
   );
--- react-complete-guide/src/components/Layout/Navigation/Navigation.js
+++ react-complete-guide/src/components/Layout/Navigation/Navigation.js
@@ -1,8 +1,20 @@
 import React from "react";
 import classes from "./Navigation.module.css";
 
-const Navigation = () => {
-  return <div className={classes.nav}>Navigation</div>;
+const Navigation = (props) => {
+  return (
+    <div className={classes.nav}>
+      <ul>
+        {props.tabs.map((tab) => (
+          <li key={tab}>
+            <button onClick={props.onClick} value={tab}>
+              {tab}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Navigation;
