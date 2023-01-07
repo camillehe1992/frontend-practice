fc4e58a63c793c56fd1dc61d79dd13ced4eda4f7
He.Camille <camille.he@outlook.com>
2023-01-07 22:05:51 +0800

    feat: remove delete comment button

--- react-blog-app/src/components/comments/CommentItem.js
+++ react-blog-app/src/components/comments/CommentItem.js
@@ -1,26 +1,9 @@
-import { useEffect, useCallback } from "react";
 import classes from "./CommentItem.module.css";
-import { deleteComment } from "../../lib/api";
-import useHttp from "../../hooks/use-http";
 
 const CommentItem = (props) => {
-  const { sendRequest } = useHttp(deleteComment);
-  const { commentId } = props;
-
-  useEffect(() => {
-    sendRequest(commentId);
-  }, [sendRequest, commentId]);
-
-  const deleteCommentHandler = useCallback(() => {
-    sendRequest(commentId);
-  }, [sendRequest, commentId]);
-
   return (
     <li className={classes.item}>
       <p>{props.text}</p>
-      <button className="btn" onClick={deleteCommentHandler}>
-        Delete
-      </button>
     </li>
   );
 };
