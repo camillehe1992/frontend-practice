b6c29c890c54a9806ab104a2bdbfcb70cec977c6
He.Camille <camille.he@outlook.com>
2023-01-10 21:08:33 +0800

    feat: use Translation to implement

--- react-animation-app/src/App.js
+++ react-animation-app/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import { Transition } from "react-transition-group";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
+const duration = 300;
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+};
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+};
+
 class App extends Component {
   state = {
+    showBlock: false,
     modalIsOpen: false,
   };
 
@@ -18,10 +33,30 @@ class App extends Component {
     this.setState({ modalIsOpen: false });
   };
 
+  reverse = () => {
+    this.setState({ showBlock: !this.state.showBlock });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={this.reverse}>
+          Toggle
+        </button>
+        <br />
+        <Transition in={this.state.showBlock} timeout={duration}>
+          {(state) => (
+            <div
+              style={{
+                ...defaultStyle,
+                ...transitionStyles[state],
+              }}
+            >
+              I'm a fade Transition!
+            </div>
+          )}
+        </Transition>
         <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         <Backdrop show={this.state.modalIsOpen} />
         <button className="Button" onClick={this.showModal}>
